065f160428347a3619a0932287ff3db0bd8d100c
ggorantla <ggorantl@eng.ucsd.edu>
2015-03-16 00:11:02 -0700

    Commiting the Files to check

---
+++ src/Lesson1_StartingThreads/Lesson1_Threads.java
@@ -0,0 +1,7 @@
+package Lesson1_StartingThreads;
+
+/**
+ * Created by ggorantla on 3/16/2015.
+ */
+public class Lesson1_Threads {
+}
---
+++ src/Lesson1_StartingThreads/Read.md
@@ -0,0 +1,5 @@
+There are several ways to start a thread in Java
+
+1.)
+2.)
+3.)
